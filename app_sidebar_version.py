9e7cae9bdd74d4574cb1f011a774c03019935ddf
Jinstall1 <128464561+Jinstall1@users.noreply.github.com>
2025-06-17 14:36:42 +0300

    Update app_sidebar_version.py

--- app_sidebar_version.py
+++ app_sidebar_version.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import json
 # Load required libraries for modeling
 from sklearn.linear_model import LinearRegression
